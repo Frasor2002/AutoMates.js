6dea63b1175b455f93b764f82b54eb92f74c7c20
WigoSas <rodrisalas17@gmail.com>
2025-06-05 19:05:00 +0200

    modified option gen

--- src/intent/utils.js
+++ src/intent/utils.js
@@ -10,19 +10,29 @@ import { aStar } from "./astar.js";
  * @param {Object} bs belief set
  * @returns priority of picking up
  */
-function priorityPickUp(parcel, bs) {
+function priorityPickUp(parcel, bs, carryingReward, nParcels) {
   // Calculate distance to parcel using A*
   const path = aStar(bs.me, parcel, bs.map);
   if(!path){ // If path is blocked
     return -Infinity; // Return lowest possible score (option will be discarded)
   }
-  const distance = path.length;
+  const px = parcel.x;
+  const py = parcel.y;
+  const distance = path.length + bs.map.deliveryMap[px][py].distance;
   // Get how much reward is lost when parcel is reached
   const timeToMove = distance * bs.config.MOVEMENT_DURATION;
-  const rewardLoss = timeToMove / bs.config.PARCEL_DECADING_INTERVAL;
+  const rewardLossRate = timeToMove / bs.config.PARCEL_DECADING_INTERVAL;
+  const rewardLoss = rewardLossRate * (nParcels + 1);
+
+  const ax = bs.me.x;
+  const ay = bs.me.y;
+  // Additional penalty so that the difference in distance counts as a penalty
+  let penaltyLoss = (distance - bs.map.deliveryMap[ax][ay].distance) * 0.3 * bs.config.PARCEL_REWARD_VARIANCE;
+  // More penalty if the starting distance is quite high
+  penaltyLoss += path.length * 0.2 * bs.config.PARCEL_REWARD_VARIANCE;
   
   // Priority is remaining reward when parcel is reached
-  let priority = parcel.reward - rewardLoss;
+  let priority = parcel.reward + carryingReward - rewardLoss - penaltyLoss;
 
   // Check if other agents are nearby the parcel
   const nearbyAgentThreshold = distance; // # tiles of tollerance
@@ -59,7 +69,7 @@ function priorityPickUp(parcel, bs) {
  * @param {Object} bs belief set
  * @returns priority of delivery
  */
-function priorityPutDown(delivery, totalReward, bs){
+function priorityPutDown(delivery, totalReward, bs, nParcels){
   const path = aStar(bs.me, delivery, bs.map, true);
   if(!path){ // Blocked path
     return -Infinity; // Return lowest possible priority
@@ -67,7 +77,9 @@ function priorityPutDown(delivery, totalReward, bs){
   // Compute how much reward is lost when moving to delivery
   const distance = path.length;
   const timeToMove = distance * bs.config.MOVEMENT_DURATION;
-  const rewardLoss = timeToMove / bs.config.PARCEL_DECADING_INTERVAL;
+  const rewardLossRate = timeToMove / bs.config.PARCEL_DECADING_INTERVAL;
+  const rewardLoss = rewardLossRate * nParcels;
+  //console.log(`DurMov:${bs.config.MOVEMENT_DURATION}\tDecad:${bs.config.PARCEL_DECADING_INTERVAL}\tDist:${distance}\tTempo:${timeToMove}\tLoss:${rewardLoss}`);
   // Remaining reward when delivery is reached multiplied by a scalar factor
   const priority = (totalReward - rewardLoss);
 
@@ -86,10 +98,15 @@ function generateOptions(bs){
   // Some parcel sometimes is corrupted without x or y and we will ignore it
   const parcels = bs.getParcels().filter(p => (!p.carriedBy || 
     p.carriedBy === bs.me.id) && p.x != null && p.y != null);
+
+  const carriedParcels = parcels.filter(p => p.carriedBy === bs.me.id);
+  const carryingReward = carriedParcels.length > 0 ? 
+    carriedParcels.reduce((sum, p) => sum + p.reward, 0) : 0;
   
+  //Checking parcels to pick up
   for(const p of parcels){
     if(!p.carriedBy){ // Parcel not carried by me
-      const priority = priorityPickUp(p, bs);
+      const priority = priorityPickUp(p, bs, carryingReward, carriedParcels.length);
       if(priority !== -Infinity){ // If option is different from -Infinity
         // Add a new option for the agent
         options.push({type: "pickUp", 
@@ -99,17 +116,14 @@ function generateOptions(bs){
     }
   }
   
-  // If I am carryig a parcel
-  const carriedParcels = parcels.filter(p => p.carriedBy === bs.me.id);
+  // If I am carrying a parcel check delivery
   if(carriedParcels.length > 0){
-    // Total reward of carried parcels
-    const totalReward = carriedParcels.reduce((sum, p) => sum + p.reward, 0);
-    
+    //console.log("ROUND OF CHECKING");
     // Generate an option for every delivery tile with different priority
     // Get only reachable delivery tiles for options
     const deliveryTiles = bs.map.filterReachableTileLists(bs.me).deliveryTiles;
     for(const delivery of deliveryTiles){
-      const priority = priorityPutDown(delivery, totalReward, bs);
+      const priority = priorityPutDown(delivery, carryingReward, bs, carriedParcels.length);
       if(priority !== -Infinity){ // Save intention only if priority is higher than -Infinity
         options.push({type:"deliver", 
           target: {x: delivery.x, y: delivery.y}, 
